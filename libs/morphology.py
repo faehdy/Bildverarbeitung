f8ba97cfcc443573534fbb6d06c75205f7f3db6c
faehdy <frischatsch@ethz.ch>
2023-05-11 16:43:19 +0200

    segment_util works

--- libs/morphology.py
+++ libs/morphology.py
@@ -16,9 +16,122 @@ def segment_util(img):
         img:        n x m x 3, values are within [0,255]
     Output:
         img_seg:    n x m
+    """ 
+    
+    
+    
+    # # Convert the image to grayscale
+    # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # # Apply Gaussian blur to reduce noise
+    # blurred = cv.GaussianBlur(gray, (5, 5), 0)
+
+    # # Apply Otsu's thresholding method to segment the image
+    # _, thresh = cv.threshold(blurred, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+    # # Perform morphological operations to remove small noise - opening
+    # # To remove holes we can use closing
+    # kernel = np.ones((3,3),np.uint8)
+    # opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations = 2)
+    # closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel, iterations = 2)
+
+    # return closing
+
+def segment_util(img):
     """
-    ## TODO
-    img_seg = ...
+    Given an input image, output the segmentation result
+    Input:  
+        img:        n x m x 3, values are within [0,255]
+    Output:
+        img_seg:    n x m
+    """
+
+    # Define an initial rectangle that includes the whole image
+    rectangle = (0, 0, img.shape[1] - 1, img.shape[0] - 1)
+
+    # Create initial mask
+    mask = np.zeros(img.shape[:2], np.uint8)
+
+    # Create two arrays used by the GrabCut algorithm internally
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+
+    # Run GrabCut algorithm
+    cv.grabCut(img, mask, rectangle, bgdModel, fgdModel, 20, cv.GC_INIT_WITH_RECT)
+
+    # Create mask where sure and likely backgrounds set to 0, otherwise 1
+    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+
+    # Multiply image with new mask to subtract background
+    img = img * mask2[:, :, np.newaxis]
+
+
+    return mask2
+
+
+
+    # Convert to 8-bit format
+    # img = np.uint8(img)
+
+    # # Define bandwidth
+    # bandwidth = 30
+
+    # # Apply mean shift
+    # img_shifted = cv.pyrMeanShiftFiltering(img, sp=bandwidth, sr=bandwidth)
+
+    # # Convert to grayscale
+    # img_seg = cv.cvtColor(img_shifted, cv.COLOR_BGR2GRAY)
+
+    # return img_seg
+
+
+
+# 
+# def segment_util(img):
+#     """
+#     Given an input image, output the segmentation result
+#     Input:  
+#         img:        n x m x 3, values are within [0,255]
+#     Output:
+#         img_seg:    n x m
+#     """
+
+#     # Reshape the image to be a simple list of RGB pixels.
+#     pixels = img.reshape(-1, 3)
+
+#     # Convert to floating point
+#     pixels = np.float32(pixels)
+
+#     # Define criteria and apply kmeans()
+#     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+#     k = 2
+#     _, labels, centers = cv.kmeans(pixels, k, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+
+#     # Convert back into uint8
+#     centers = np.uint8(centers)
+
+#     # Map the labels to centers
+#     segmented_image = centers[labels.flatten()]
+
+#     # Reshape back to the original image
+#     segmented_image = segmented_image.reshape(img.shape)
+
+#     # Convert color to grayscale
+#     img_seg = cv.cvtColor(segmented_image, cv.COLOR_BGR2GRAY)
+
+#     return img_seg
+
+
+# def segment_util(img):
+#     """
+#     Given an input image, output the segmentation result
+#     Input:  
+#         img:        n x m x 3, values are within [0,255]
+#     Output:
+#         img_seg:    n x m
+#     """
+#     ## TODO
+#     img_seg = ...
 
     return img_seg
 
